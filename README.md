cae69fcacfe1b1a2d9baad9be479d5b22687a087
gary909 <garywhite61@hotmail.com>
2021-01-06 15:05:37 +0100

    code changes

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Chapter 9 - Sticky notes App
 
 Gen 1:
 
-
-
 ![Version 1 of Stickies](./StickiesGen1.png)
+
+Gen 3:
+
+![Version 3 of Stickies](./stickiesGen3.png)
--- notetoself.js
+++ notetoself.js
@@ -1,5 +1,12 @@
-window.onload = init;
+/* notetoself.js
+ *
+ * This version uses the array and allows delete of the sticky note
+ * Also includes a Clear all notes button that clears out everything
+ * in local storage, removes sticky notes from the DOM and resets the 
+ * stickies array to []
+ */
 
+window.onload = init;
 
 function init() {
 	var button = document.getElementById("add_button");
@@ -28,7 +35,6 @@ function init() {
 	}	
 }
 
-
 function getStickiesArray() {
 	var stickiesArray = localStorage.getItem("stickiesArray");
 	if (!stickiesArray) {
@@ -38,16 +44,6 @@ function getStickiesArray() {
 		stickiesArray = JSON.parse(stickiesArray);
 	}
 	return stickiesArray;
-}
-
-
-function addStickyToDOM(value) {
-    var stickies = document.getElementById("stickies");
-    var sticky = document.createElement("li");
-    var span = document.createElement("span");
-    span.innerHTML = value;
-    sticky.appendChild(span);
-    stickies.appendChild(sticky);
 }
 
 function createSticky() {
@@ -61,5 +57,55 @@ function createSticky() {
 	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
 	
 	addStickyToDOM(key, value);
+}
+
+function deleteSticky(e) {
+	var key = e.target.id;
+	if (e.target.tagName.toLowerCase() == "span") {
+		key = e.target.parentNode.id;
+	}
+	localStorage.removeItem(key);
+	var stickiesArray = getStickiesArray();
+	if (stickiesArray) {
+		for (var i = 0; i < stickiesArray.length; i++) {
+			if (key == stickiesArray[i]) {
+				stickiesArray.splice(i,1);
+			}
+		}
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+		removeStickyFromDOM(key);
+	}
+}
+
+
+function addStickyToDOM(key, value) {
+	var stickies = document.getElementById("stickies");
+	var sticky = document.createElement("li");
+	sticky.setAttribute("id", key);
+	var span = document.createElement("span");
+	span.setAttribute("class", "sticky");
+	span.innerHTML = value;
+	sticky.appendChild(span);
+	stickies.appendChild(sticky);
+
+	sticky.onclick = deleteSticky;
+}
+
+function removeStickyFromDOM(key) {
+	var sticky = document.getElementById(key);
+	sticky.parentNode.removeChild(sticky);
+}
+
+
+function clearStickyNotes() {
+	localStorage.clear();
+	var stickyList = document.getElementById("stickies");
+	var stickies = stickyList.childNodes;
+	for (var i = stickies.length-1; i >= 0; i--) {
+		stickyList.removeChild(stickies[i]);
+	}
+
+	// reset notes array
+	var stickiesArray = getStickiesArray();
 }
 
